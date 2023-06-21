0ad07831a0fef53708c1c326c3bc765dcb645003
Giuliana Cubilla <giulii024.4@gmail.com>
2023-06-21 15:11:25 -0300

    se creó un modelo de datos para las reservas.

--- server/models/Reserva.js
+++ server/models/Reserva.js
@@ -1,1 +1,19 @@
 // TODO: Crear modelo de datos de Reserva
+const express = require('express');
+const router = express.Router();
+const Reserva = require('./models/Reserva.js');
+
+// Ruta para crear una reserva
+router.post('/', (req, res) => {
+  const { nombre, apellido, dni, codigo, fecha } = req.body;
+
+  Reserva.create({ nombre, apellido, dni, codigo, fecha })
+    .then(reserva => {
+      res.status(201).json(reserva);
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error al crear la reserva' });
+    });
+});
+
+module.exports = router;
