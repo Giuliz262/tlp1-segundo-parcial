6b5831c4242e5d3defa84aac4e49b66a42612f77
Giuliana Cubilla <giulii024.4@gmail.com>
2023-06-21 16:09:42 -0300

    se creó la estructura para implementar las rutas tanto para visualizar las vistas,como para manejar el CRUD de reservas.

--- server/routes/reserva.routes.js
+++ server/routes/reserva.routes.js
@@ -1,33 +1,39 @@
-// TODO: Importar el modelo y controladores de reservas, luego vincular rutas con controladores
+const express = require('express');
+const router = express.Router();
 
-const router = require('express').Router();
-
+// Importar el modelo y controladores de reservas
+const Reserva = require('../models/Reserva');
+const reservaController = require('../controllers/reserva.controllers');
 
 // ==========================================
 //         Rutas para renderizar vistas
 // ==========================================
 
 // Obtener todas las reservas
+router.get('/', reservaController.obtenerReservasVista);
 
 // Formulario para crear una reserva
+router.get('/crear', reservaController.mostrarFormularioCrearReserva);
 
 // Formulario para actualizar una reserva
+router.get('/editar/:id', reservaController.mostrarFormularioActualizarReserva);
 
 // ==========================================
 //         Rutas para CRUD de reservas
 // ==========================================
 
 // Obtener todas las reservas
-router.get('/api/',);
- 
+router.get('/api/reservas', reservaController.obtenerReservas);
+
 // Crear una reserva
-router.post('/api/',);
- 
+router.post('/api/reservas', reservaController.crearReserva);
+
 // Actualizar una reserva
-router.put('/api/:id',);
- 
+router.put('/api/reservas/:id', reservaController.actualizarReserva);
+
 // Eliminar una reserva de forma lógica
-router.delete('/api/:id',);
+router.delete('/api/reservas/:id', reservaController.eliminarReserva);
+
+module.exports = router;
 
  
- module.exports = router;
