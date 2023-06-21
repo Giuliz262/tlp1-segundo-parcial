7be4ada6b33b1cfbdc99fec38671bd30cff06a13
Giuliana Cubilla <giulii024.4@gmail.com>
2023-06-21 15:03:19 -0300

    se configuró el archivo .env para implementar variables de entorno.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
-// Imports
+require('dotenv').config();
+
 const cors = require('cors');
 const express = require('express');
-
 const path = require('path');
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT
+});
 
 const app = express();
 
@@ -17,4 +24,4 @@ app.use('/api', require('./routes/reserva.routes'));
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
